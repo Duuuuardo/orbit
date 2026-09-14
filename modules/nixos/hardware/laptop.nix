5b02fb60466fe8aff5b666b05d4de0f448c40186
Eduardo <eduardo@voyager.local>
2026-09-14 14:05:32 -0300

    feat: Remove prime

--- modules/nixos/hardware/laptop.nix
+++ modules/nixos/hardware/laptop.nix
@@ -10,14 +10,5 @@
     branch = "legacy_580";
     nvidiaSettings = true;
     powerManagement.enable = true;
-
-    prime.offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-
-    # TODO: fill after running nixos-generate-config
-    # prime.intelBusId = "PCI:0:2:0";
-    # prime.nvidiaBusId = "PCI:1:0:0";
   };
 }
